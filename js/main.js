061572d63fdd89477a6d028e3de3a5eda9791096
Aaron Mak Kang Sheng <aaronmakks@gmail.com>
2016-10-19 17:22:29 +0800

    add point in states

--- js/main.js
+++ js/main.js
@@ -10,6 +10,8 @@ var svg = d3.select("body").insert("svg:svg", "h2")
     .attr("width", w)
     .attr("height", h);
 
+var centroidPositions = [];
+
 var states = svg.append("svg:g")
     .attr("id", "states");
 
@@ -26,7 +28,7 @@ d3.json("data/us-states.json", function(collection) {
       .attr("d", path);
 });
 
-function sumArrayCSV(array,prop,condition) {
+function sumArrayCSV(array,prop) {
   var sum = 0;
   for (i=0; i<array.length; i++) {
     sum += parseFloat(array[i][prop]);
@@ -39,6 +41,36 @@ d3.csv("data/passengers-summarized.csv", function(fare) {
 
   // var radius = d3.scale.sqrt()
   //             .domain([0,])
+});
+
+d3.json("data/us-states-centroids.json", function(json) {
+
+  var centroids = json.features.filter(function(centroid) {
+    var location = [+centroid.geometry.coordinates[0], +centroid.geometry.coordinates[1]];
+    var projectedLocation = projection(location);
+    if (projectedLocation !== null) {
+      centroidPositions.push(projectedLocation);
+    }
+  });
+
+  circles.selectAll("circle")
+    .data(json.features)
+    .enter()
+    .append("circle")
+    .attr("r",2)
+    .attr("cx", function(d, i) {
+      // return centroidPositions[i][0];
+      var location = projection([json.features[i].geometry.coordinates[0],json.features[i].geometry.coordinates[1]]);
+      if (location !== null) {
+        return location[0];
+      }
+    })
+    .attr("cy", function(d, i) {
+      var location = projection([json.features[i].geometry.coordinates[0],json.features[i].geometry.coordinates[1]]);
+      if (location !== null) {
+        return location[1];
+      }
+    });
 });
 
 // d3.csv("flights-airport.csv", function(flights) {
